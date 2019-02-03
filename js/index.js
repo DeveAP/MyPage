88661e2aad331f4e0bc35fcf8ce950d9c6f15727
AndPost_Dell <deveandres@gmail.com>
2019-02-03 18:45:19 -0500

    Implementacion de interacion con el evento touchmove en el apartado movil

--- js/index.js
+++ js/index.js
@@ -58,4 +58,41 @@ function all_fill(contents){
         contents[i].style.display = 'inherit';
     }
 }
+// skills
+const slide =document.getElementsByClassName('skills-content');
+let move = 0; // valor determinante de la pantalla 
+let num = 0; // valor de ubicacion del movimiento
+const acelerador = 8; // acelera
+
+// slide[0].addEventListener('touchstart',(e)=>{ // Evento que ubica la pantalla 
+//     move = e.changedTouches[0].clientX
+//     console.log(move)
+// }) 
+slide[0].addEventListener('touchmove',(e) => { // Evento que mueve los elementos
+    let moving = e.changedTouches[0].clientX
+    if(move > moving){
+        console.log('izquierda')
+        if(num >= -584){
+            num -= acelerador;
+            slide[0].style.left = `${num}px`
+        }
+        else{
+            console.log('block')
+        }
+    }
+    else{
+        console.log('Derecha')
+        if (num <= 0) {
+            num += acelerador;
+            slide[0].style.left = `${num}px`        
+        }
+        else{
+            console.log('block')
+        }
+    }
+    move = moving // declaracion del punto final del movimiento 
+    
+})
+
+ 
 
