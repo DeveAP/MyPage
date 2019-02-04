7bc1b63f301af6cb485eaafc803856c3abea8aca
AndPost_Dell <deveandres@gmail.com>
2019-02-03 19:11:27 -0500

    Ajustes en la limitacion del contenedor del slide de los skills

--- js/index.js
+++ js/index.js
@@ -63,16 +63,18 @@ const slide =document.getElementsByClassName('skills-content');
 let move = 0; // valor determinante de la pantalla 
 let num = 0; // valor de ubicacion del movimiento
 const acelerador = 8; // acelera
-
+let max = 0;
+let min = 0;
 // slide[0].addEventListener('touchstart',(e)=>{ // Evento que ubica la pantalla 
 //     move = e.changedTouches[0].clientX
 //     console.log(move)
 // }) 
 slide[0].addEventListener('touchmove',(e) => { // Evento que mueve los elementos
     let moving = e.changedTouches[0].clientX
+    max = slide[0].children[0].scrollWidth * 3.3
     if(move > moving){
         console.log('izquierda')
-        if(num >= -584){
+        if(num >= -max){
             num -= acelerador;
             slide[0].style.left = `${num}px`
         }
@@ -82,7 +84,7 @@ slide[0].addEventListener('touchmove',(e) => { // Evento que mueve los elementos
     }
     else{
         console.log('Derecha')
-        if (num <= 0) {
+        if (num <= min) {
             num += acelerador;
             slide[0].style.left = `${num}px`        
         }
