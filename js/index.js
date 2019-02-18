e03cac1c8ccaea93d41545ada3efbb3939f1c39d
AndPost_Dell <deveandres@gmail.com>
2019-02-18 03:32:30 -0400

    Madificacion de animaciones,adiccion de id en el menu y correccion en el correo

--- js/index.js
+++ js/index.js
@@ -4,30 +4,37 @@ const menu = document.getElementsByClassName('header-style');
 const about = document.getElementsByClassName('about-me')[0];
 const skill = document.getElementsByClassName('content-skills')[0];
 const project = document.getElementsByClassName('content-adjust')[0];
-let count = [0,0,0]
+const portal = document.getElementsByClassName('portal-text')[0];
+let count = [0,0,0,0]
 window.onscroll = () => {
-    console.log(window.scrollY)
-    if(window.scrollY > 51 && window.scrollY < 1304 && count[0] == 0){
+    if(window.scrollY > 51 && window.scrollY < 1304 && count[0] == 0){ // Animacion de la biografia 
         about.animate(animation_left,{
             duration:1000,
             fill:'forwards'
         });
         count[0] = 1;
     }
-    if(window.scrollY > 741 && window.scrollY < 2092 && count[1] == 0){
+    if(window.scrollY > 741 && window.scrollY < 2092 && count[1] == 0){ // Animacion de habilidades
         skill.animate(animation_top,{
             duration: 1000,
             fill: 'forwards'
         })
         count[1] = 1;
     }
-    if(window.scrollY > 1904 && count[2] == 0){
+    if(window.scrollY > 1904 && count[2] == 0){ // Animacion de proyectos
         project.animate(animation_right,{
             duration: 2000,
             fill: 'forwards'
         })
         count[2] = 1;
-    }      
+    } 
+    if(window.scrollY > -1 && window.scrollY < 51 && count[3] == 0){ // Animacion de portal 
+        portal.animate(animation_left,{
+            duration:1000,
+            fill:'forwards'
+        });
+        count[3] = 1;
+    }     
     if (window.scrollY > 200) {
         menu[0].classList = "header-style active-menu";
     }
